84539b3f9d4761e9633f41bbcb51a3407fbe82d1
Carl V <vogel.carl.l@gmail.com>
2023-09-18 17:09:27 +0200

    restructured code into functions, made saving and showing the 3D model asynchronous to reduce lag

--- lidar_script.py
+++ lidar_script.py
@@ -1,32 +1,49 @@
 import open3d as o3d
-import numpy as np
+import asyncio
 
-# Load the LIDAR point cloud
-print("load LIDAR point cloud")
-lidar = o3d.io.read_point_cloud("ply_scan/Scan_20_24_51.ply") 
 
-# Downsample the point cloud
-print("Downsampling")
-lidar_down = lidar.voxel_down_sample(voxel_size=0.02)
+def load_data(path="ply_scan/Scan_20_24_51.ply"):
+    # Load the LIDAR point cloud
+    print("load LIDAR point cloud")
+    return o3d.io.read_point_cloud(path) 
 
-# Remove statistical outliers
-cl, ind = lidar_down.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+def edit_data(model):
+    # Downsample the point cloud
+    print("Downsampling")
+    lidar_down = model.voxel_down_sample(voxel_size=0.02)
+    # Remove statistical outliers
+    print("remove statistical outliers from the point cloud")
+    cl, ind = lidar_down.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+    # Select the inlier points
+    inlier_cloud = lidar_down.select_by_index(ind)
+    # Surface reconstruction
+    print("reconstruct surface")
+    poisson_mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(inlier_cloud, depth=9)
+    # Create a wireframe representation of the mesh and color the wireframe
+    print("create wireframe")
+    wireframe = o3d.geometry.LineSet.create_from_triangle_mesh(poisson_mesh)
+    wireframe.paint_uniform_color([1, 0.41, 0.71])  # RGB-color value for pink
+    
+    return poisson_mesh, wireframe
 
-# Select the inlier points
-inlier_cloud = lidar_down.select_by_index(ind)
+async def show_model(poisson_mesh, wireframe):
+    # Visualize the reconstructed mesh and its wireframe
+    print("visualizing...")
+    o3d.visualization.draw_geometries([poisson_mesh, wireframe])
 
-# Surface reconstruction
-poisson_mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(inlier_cloud, depth=9)
+async def save_data_gltf(poisson_mesh):
+    # Save the reconstructed mesh as a glTF file
+    print("Save file as Graphics Library Transmission Format (.glTF)")
+    o3d.io.write_triangle_mesh("gltf_3d_mash/reconstructed_mesh.gltf", poisson_mesh)
 
-# Create a wireframe representation of the mesh
-wireframe = o3d.geometry.LineSet.create_from_triangle_mesh(poisson_mesh)
+def main():
+    path = input("path to your ply-data")
+    model = load_data(path)
+    poisson_mesh, wireframe = edit_data(model)
+    show_model(poisson_mesh, wireframe)
+    save_data_gltf(poisson_mesh)
+    asyncio.gather()
+    print("your glTF file has been saved to '/gltf_3d_mash/reconstructed_mesh.gltf'")
 
-# Set the color of the wireframe to red (RGB: [255, 0, 0])
-wireframe.paint_uniform_color([1, 0.41, 0.71])  # RGB-color value for pink
-
-# Visualize the reconstructed mesh and its wireframe
-o3d.visualization.draw_geometries([poisson_mesh, wireframe])
-
-# Save the reconstructed mesh as a glTF file
-print("Save file as Graphics Library Transmission Format (.glTF)")
-o3d.io.write_triangle_mesh("gltf_3d_mash/reconstructed_mesh.gltf", poisson_mesh)
+if __name__ == '__main__':
+    main()
